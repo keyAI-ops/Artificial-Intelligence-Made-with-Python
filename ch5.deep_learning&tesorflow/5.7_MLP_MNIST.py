79ff2a1d942bef7a08c21abbd00fa48106d2b2e3
Chang_K <ckckckck1207@naver.com>
2022-05-14 18:16:20 +0900

    mlp_mnist_dataset

--- ch5.deep_learning&tesorflow/5.7_MLP_MNIST.py
+++ ch5.deep_learning&tesorflow/5.7_MLP_MNIST.py
@@ -1,1 +1,29 @@
-print('hello')
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(60000,784)
+x_test = x_test.reshape(10000,784)
+x_train = x_train.astype(np.float32)/255.0
+x_test = x_test.astype(np.float32)/255.0
+y_train = tf.keras.utils.to_categorical(y_train,10)
+y_test = tf.keras.utils.to_categorical(y_test,10)
+
+n_input = 784
+n_hidden = 1024
+n_output = 10
+
+mlp = Sequential()
+mlp.add(Dense(units=n_hidden, activation='tanh',input_shape=(n_input,),kernel_initializer='random_uniform',bias_initializer='zeros'))
+mlp.add(Dense(units=n_output, activation='tanh',kernel_initializer='random_uniform',bias_initializer='zeros'))
+
+mlp.compile(loss='mean_squared_error',optimizer=Adam(learning_rate=0.001),metrics=['accuracy'])
+hist = mlp.fit(x_train,y_train,batch_size=128,epochs=30,validation_data=(x_test,y_test),verbose=1)
+
+res = mlp.evaluate(x_test,y_test,verbose=0)
+print("정확률은", res[1]*100)
